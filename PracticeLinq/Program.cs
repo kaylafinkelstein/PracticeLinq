dca59b6289acca5cb071f496d8ea43546c1b2b4b
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-05-29 01:32:19 -0400

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> videoGames = new List<string>() {"Animal Crossing", "Halo", "Elden Ring"};
+
+            var games = videoGames.OrderBy(x => x.Length);
+
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
